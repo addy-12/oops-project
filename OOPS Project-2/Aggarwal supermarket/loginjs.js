e3253e0c55313dc358d31577bf4e8767fd94e8d1
Aditya Aggarwal <adityasaiaggarwal@gmail.com>
2022-12-03 19:05:38 +0530

    made changes

--- OOPS Project-2/Aggarwal supermarket/loginjs.js
+++ OOPS Project-2/Aggarwal supermarket/loginjs.js
@@ -1,14 +1,17 @@
 
-const userBtn= document.querySelector(".userBtn");
-const adminBtn= document.querySelector(".adminBtn");
-const moveBtn= document.querySelector(".moveBtn");
-const user= document.querySelector(".user");
-const admin= document.querySelector(".admin");
+const userBtn = document.querySelector(".userBtn");
+const adminBtn = document.querySelector(".adminBtn");
+const moveBtn = document.querySelector(".moveBtn");
+const managerBtn = document.querySelector(".managerBtn");
+const user = document.querySelector(".user");
+const admin = document.querySelector(".admin");
+const manager = document.querySelector(".manager");
 
 adminBtn.addEventListener('click',()=>{
     moveBtn.classList.add("rigthBtn");
     admin.classList.add("adminForm");
     user.classList.remove("userForm");
+    manager.classList.remove("managerForm");
     moveBtn.innerHTML="Admin";
 
 });
@@ -16,5 +19,13 @@ userBtn.addEventListener('click',()=>{
     moveBtn.classList.remove("rigthBtn");
     admin.classList.remove("adminForm");
     user.classList.add("userForm");
+    manager.classList.remove("managerForm");
     moveBtn.innerHTML="User";
+});
+managerBtn.addEventListener('click',()=>{
+    moveBtn.classList.add("rigthBtn2");
+    admin.classList.remove("adminForm");
+    user.classList.remove("userForm");
+    manager.classList.add("managerForm");
+    moveBtn.innerHTML="Manager";
 });
