61876bb61ab3e38ee9d8483410a952a2dcc7b85a
Aditya Aggarwal <adityasaiaggarwal@gmail.com>
2022-12-05 16:08:11 +0530

    login page updated

--- OOPS Project-2/Aggarwal supermarket/loginjs.js
+++ OOPS Project-2/Aggarwal supermarket/loginjs.js
@@ -9,21 +9,24 @@ const manager = document.querySelector(".manager");
 
 adminBtn.addEventListener('click',()=>{
     moveBtn.classList.add("rigthBtn");
+    moveBtn.classList.remove("rigthBtn2");    
     admin.classList.add("adminForm");
     user.classList.remove("userForm");
     manager.classList.remove("managerForm");
     moveBtn.innerHTML="Admin";
-
 });
 userBtn.addEventListener('click',()=>{
     moveBtn.classList.remove("rigthBtn");
+    moveBtn.classList.remove("rigthBtn2");    
     admin.classList.remove("adminForm");
     user.classList.add("userForm");
+    manager.classList.remove("managerForm");
     moveBtn.innerHTML="User";
 });
 managerBtn.addEventListener('click',()=>{
     moveBtn.classList.add("rigthBtn2");
+    moveBtn.classList.remove("rigthBtn");
     admin.classList.remove("adminForm");
-    user.classList.remove("userForm");
+    manager.classList.add("managerForm");
     moveBtn.innerHTML="Manager";
 });
